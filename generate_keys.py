c544b951176a89a0b56e9ce51c5274e98e4cc548
x011 <x011@users.noreply.github.com>
2024-02-04 01:41:05 +0000

    Add files via upload

--- generate_keys.py
+++ generate_keys.py
@@ -1,6 +1,10 @@
-from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives.hashes import SHA512
+from cryptography.hazmat.backends import default_backend
 from getpass import getpass
+import os
 
 def generate_keys():
     password = getpass("RSA Private Key Passphrase:").encode('utf-8')
@@ -9,9 +13,22 @@ def generate_keys():
     private_key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=4096,
+        backend=default_backend()
     )
     
-    # Serialize private key with passphrase
+    # Generate a random salt
+    salt = os.urandom(16)
+    
+    # Define PBKDF2HMAC parameters
+    kdf = PBKDF2HMAC(
+        algorithm=SHA512(),
+        length=32,
+        salt=salt,
+        iterations=131072,
+        backend=default_backend()
+    )
+    
+    # Serialize private key with passphrase and PBKDF2HMAC parameters
     pem_private = private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
