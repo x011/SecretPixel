7afcd27b431cf698dab863e227f8755189138e2e
x011 <x011@users.noreply.github.com>
2024-02-04 01:31:28 +0000

    Add files via upload

--- generate_keys.py
+++ generate_keys.py
@@ -1,21 +1,38 @@
-from Crypto.PublicKey import RSA
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
 from getpass import getpass
 
-mykey = RSA.generate(4096)
+def generate_keys():
+    password = getpass("RSA Private Key Passphrase:").encode('utf-8')
+    
+    # Generate private key
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=4096,
+    )
+    
+    # Serialize private key with passphrase
+    pem_private = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.BestAvailableEncryption(password)
+    )
+    
+    # Write private key to file
+    with open("myprivatekey.pem", "wb") as f:
+        f.write(pem_private)
+    
+    # Generate public key
+    public_key = private_key.public_key()
+    
+    # Serialize public key
+    pem_public = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    
+    # Write public key to file
+    with open("mypublickey.pem", "wb") as f:
+        f.write(pem_public)
 
-pwd = getpass("RSA Private Key Passphrase:").encode('utf-8')
-
-with open("myprivatekey.pem", "wb") as f:
-
-	data = mykey.export_key(passphrase=pwd,
-	pkcs=8,
-	protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
-	prot_params={'iteration_count':131072})
-
-	f.write(data)
-
-
-with open("mypublickey.pem", "wb") as f:
-
-    data = mykey.public_key().export_key()
-    f.write(data)
+generate_keys()
